93cc799efdf3b156e65a5b4e20a11398b7854c67
hector <hectorrrivera1010@gmail.com>
2024-03-02 23:48:18 -0600

    [add] components

---
+++ app/util/Data.ts
@@ -0,0 +1,23 @@
+interface Image {
+  date: string;
+  image: string;
+  description: string;
+}
+
+export const ENTRIES: Image[] = [
+  {
+    date: "2024-march-01",
+    image: "/practices/01.webp",
+    description: "trying body anatomy and later some anime.",
+  },
+  {
+    date: "2024-march-01",
+    image: "/practices/01.webp",
+    description: "trying body anatomy and later some anime.",
+  },
+  {
+    date: "2024-march-01",
+    image: "/practices/01.webp",
+    description: "trying body anatomy and later some anime.",
+  },
+];
