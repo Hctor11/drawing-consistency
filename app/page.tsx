ad70c04f7128c4b73e4eaebf1752c9955a0bb297
hector <hectorrrivera1010@gmail.com>
2024-03-02 23:57:53 -0600

    [add] layout test 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import Gallery from "@/components/Gallery";
 
 export default function Home() {
   return (
-    <main className="min-h-screen justify-between p-12">
+    <main className="min-h-screen justify-between p-2 sm:p-4 md:p-6 lg:p-8">
      <h1 className="font-bold text-xl">Drawing practices</h1>
      <p>All of my drawing practices and my evolution in my drawing technique.</p>
+     <Gallery/>
     </main>
   );
 }
--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,8 +1,15 @@
-
+import { ENTRIES } from "@/util/Data"
+import Image from "next/image"
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
+    <div className="columns-1 gap-2 sm:columns-2 sm:gap-3 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
+      {ENTRIES.map((item)=>(
+        <>
+          <Image src={item.image}  width={200} height={300} alt={item.description} className="rounded-md" />
+        </>
+      ))}
+    </div>
   )
 }
 
